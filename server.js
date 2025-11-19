83f0f01130ffd50376ed6a785e93ef217d439275
invika <invikareddy@gmail.com>
2025-11-19 14:54:57 -0600

    Added HTML response

--- server.js
+++ server.js
@@ -16,6 +16,6 @@ const server = http.createServer((req, res) => {
   });
 });
 
-server.listen(3000, () => {
+server.listen(3000, "0.0.0.0", () => {
   console.log("Server running on port 3000!");
 });
