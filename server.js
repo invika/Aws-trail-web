99d38136ed1c9d05078ddae08f8d0f7521a7264a
invika <invikareddy@gmail.com>
2025-11-19 12:43:37 -0600

    Added name to HTML response

--- server.js
+++ server.js
@@ -16,12 +16,20 @@ const server = http.createServer((req, res) => {
         </script>
       </head>
       <body>
-        <h1>Hello from AWS EC2 and GitHub!</h1>
-        <p>This is a small interactive message demo.</p>
-        <img src="https://static.vecteezy.com/system/resources/previews/012/744/791/large_2x/cute-girl-working-on-computer-cartoon-icon-illustration-people-and-technology-icon-concept-isolated-premium-flat-cartoon-style-vector.jpg" 
-             alt="Cartoon girl working on computer" 
-             width="400" />
-      </body>
+  <h1>Hello from Invi's AWS EC2 and GitHub!</h1>
+  <p>This is a small interactive message demo.</p>
+
+  <div style="border: 5px solid #333; 
+              padding: 10px; 
+              width: fit-content; 
+              box-shadow: 4px 4px 12px rgba(0,0,0,0.3); 
+              border-radius: 8px;">
+    <img src="https://static.vecteezy.com/system/resources/previews/012/744/791/large_2x/cute-girl-working-on-computer-cartoon-icon-illustration-people-and-technology-icon-concept-isolated-premium-flat-cartoon-style-vector.jpg" 
+         alt="Cartoon girl working on computer" 
+         width="400" />
+  </div>
+</body>
+
     </html>
   `);
 });
